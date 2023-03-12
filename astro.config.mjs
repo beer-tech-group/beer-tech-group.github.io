410d70adbcc92df398316f3882e444881e7dc336
Francesca Motisi <fsca.motisi@gmail.com>
2023-03-12 17:48:29 +0100

    feat: render single event (#4)

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,29 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), mdx(), tailwind()]
+  site: 'https://beertechgroup.io',
+  integrations: [preact(), mdx(), tailwind(), compress(), robotsTxt(), sitemap({
+    changefreq: 'monthly',
+    priority: 0.7,
+    lastmod: new Date(),
+    i18n: {
+      defaultLocale: 'it',
+      locales: {
+        it: 'it-IT'
+      }
+    }
+  }), image()]
 });
