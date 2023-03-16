a90ba5802c4fba9abc911ad5099146cb8c3e3eb3
Francesca Motisi <fsca.motisi@gmail.com>
2023-03-16 23:46:26 +0100

    feat: add seo configuration (#18)

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/image/client" />
+
+interface ImportMetaEnv {
+  readonly SITE_NAME: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
